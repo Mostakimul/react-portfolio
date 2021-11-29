374c0431b835f31affe4bd5581abe56dac526f1b
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-29 02:22:00 +0100

    Basci home page

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h2>Home section</h2>
+    <div className="par h-full grid place-content-center text-center space-y-3">
+      <h1 className="text-6xl font-semibold">Mostakimul Karim</h1>
+      <h2 className="text-3xl">Full Stack Developer...</h2>
     </div>
   );
 };

60ba2f03aa62724d6aa4abfaba53d50d0244a131
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-29 22:45:12 +0100

    firebase inital deploy

--- src/component/About.js
+++ src/component/About.js
@@ -8,6 +8,7 @@ const About = () => {
         title="About Me"
         iconShape="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
       />
+      <h3 className="text-center font-semibold">Coming Soon!</h3>
     </div>
   );
 };
--- src/component/Education.js
+++ src/component/Education.js
@@ -8,6 +8,7 @@ const Education = () => {
         title="Education"
         iconShape="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
       />
+      <h3 className="text-center font-semibold">Coming Soon!</h3>
     </div>
   );
 };

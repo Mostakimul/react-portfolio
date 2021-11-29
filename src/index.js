5fbee173fbc59588f6e1e9e91026e4df6ebe22cf
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-29 11:04:00 +0100

    react loader spinner imported

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';

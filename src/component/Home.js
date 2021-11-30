a810dd5f35b52a487b8f75333d62f42bafd80ba8
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-30 03:12:38 +0100

    react typical setup

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Typical from 'react-typical';
 
 const Home = () => {
   return (
     <div className="h-full grid place-content-center text-center">
       <h1 className="text-4xl sm:text-5xl md:text-6xl font-semibold my-5">Mostakimul Karim</h1>
-      <h2 className="text-xl sm:text-2xl md:text-3xl mb-5">Full Stack Developer</h2>
+      <h2 className="text-xl sm:text-2xl md:text-3xl mb-5">
+        <Typical
+          steps={['I am', 1000, 'I am Fullstack Developer...', 500]}
+          loop={Infinity}
+          wrapper="p"
+        />
+      </h2>
       <hr />
       <div className="mt-5">
         <a
